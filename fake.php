68bec777cefaf4004a298b10bffec7741cb5c1d4
rohitkumar05 <rohit.hmsadroit@gmail.com>
2019-04-24 14:08:45 +0530

    today

--- fake.php
+++ fake.php
@@ -1,45 +1,20 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h2>HTML Forms</h2>
-
-<form action="fake.php">
-  First name:<br>
-  <input type="text" name="name" placeholder="eneter name">
-  <br>
-  Last name:<br>
-  <input type="text" name="email"placeholder="enter email" >
-  <br><br>
-  <input type="submit" value="Submit">
-</form> 
-
-
-
-</body>
-</html>
-
 <?php
-
-if(isset($_POST['submit']))
-{
-	$a=$_POST['name'];
-	$b=$_POST['email'];
-
+if(isset($_POST['submit'])) {
+	$a=$_POST['id_t'];
+	$b=$_POST['name'];
+	$c=$_POST['email'];
+	
 
 
 $conn = new mysqli("localhost","root","hms@2019","nikhil");
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully"; 	
 
-
-$sql="insert into tbl_nikhil(name,email)values('$a','$b')";
-
-
-
-
+$sql="insert into tbl_nikhil(id_t,name,email)values('$a','$b','$c')";
 
 
 
@@ -51,3 +26,32 @@ $sql="insert into tbl_nikhil(name,email)values('$a','$b')";
 
 $conn->close();
 }
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form method="post" action="">
+
+   user id:<br>
+  <input type="text" name="id_t" placeholder="eneter id">
+  <br>
+  First name:<br>
+  <input type="text" name="name" placeholder="eneter name">
+  <br>
+  Last name:<br>
+  <input type="text" name="email"placeholder="enter email" >
+  <br>
+  <input type="submit" class="btn btn-default" value="SUBMIT" name="submit">
+</form> 
+
+
+
+</body>
+</html>
+

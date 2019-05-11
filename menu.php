6176a0c4866976b1e342672018ac31d3f2f8f118
rohitkumar05 <rohit.hmsadroit@gmail.com>
2019-05-11 15:45:54 +0530

    menu

---
+++ menu.php
@@ -0,0 +1,29 @@
+<?php include_once('./partials/header.php'); ?> 
+
+<h1>MENU</h1>
+<h3>Select what you want to eat</h3>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="col-sm-4">
+      <h3>Veg</h3>
+      <a href="veg.php"><img src="http://localhost/first_demo/1e06e19dff772dfc3e7a51caaddd16a5.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Fast-Food</h3> 
+      <a href="fast_food.php"><img src="http://localhost/first_demo/xjmx1csashjww8j8jwyh.webp" alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>    
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+
+<?php include_once('./partials/footer.php');?>
---
+++ veg.php
@@ -0,0 +1,94 @@
+<?php include_once('./partials/header.php'); ?> 
+<h1>MENU</h1>
+<h3>Select what you want to eat</h3>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Non-Veg</h3>
+      <a href="non_veg.php"><img src="http://localhost/first_demo/images.jpeg"  alt="Cinque Terre"  class="mx-auto d-block img-thumbnail" alt="Cinque Terre" style="width:50%"></a>
+      <p>This Restaurant Provide All Type Of Non-Veg</p>
+      
+    </div>
+    
+
+<?php include_once('./partials/footer.php');?>
